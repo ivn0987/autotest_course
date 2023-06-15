7bf869cd1cb6359ab7a820d4a0df5f81cb7b6610
Иван <iv.nikolaev@tensor.ru>
2023-06-15 10:16:10 +0300

    Исправил локаторы

--- task_2.py
+++ task_2.py
@@ -47,46 +47,15 @@ try:
     time.sleep(2)
     driver.find_element(By.XPATH, '//div[@data-qa="textEditor_slate_Field"]').send_keys('Текстовое сообщение')
     time.sleep(2)
-    driver.find_element(By.XPATH, '//span[@data-qa="msg-send-editor__send-button"]'
-                                  '/span[@class="controls-BaseButton__wrapper controls-Button__wrapper_viewMode-filled '
-                                  'controls-BaseButton__wrapper_captionPosition_end controls-Button_textAlign-center '
-                                  'controls-Button__wrapper_filled_l"]').click()
+    driver.find_element(By.XPATH, '//span[@data-qa="msg-send-editor__send-button"]').click()
     time.sleep(2)
-    if driver.find_element(By.XPATH, '//div[@class="msg-entity-text msg-entity_font_croppless '
-                                     'richEditor_richContentRender_fontSize-m_theme-default '
-                                     'controls_RichEditor_theme-default richEditor_richContentRender_theme-default '
-                                     'richEditor_richContentRender richEditor_richContentRender_lineHeight-s '
-                                     'richEditor_richContentRender_colorPalette-first '
-                                     'richEditor_richContentRender_readOnly msg-dialogs-item__message-text '
-                                     'msg-entity-text_normalized controls-List_DragNDrop__notDraggable '
-                                     'ws-flex-shrink-1 msg-entity-expander__content '
-                                     'ws-flex-grow-1 ws-flex-shrink-1"]').text == 'Текстовое сообщение':
-        driver.find_element(By.XPATH, '//div[@class="msg-entity-text msg-entity_font_croppless '
-                                      'richEditor_richContentRender_fontSize-m_theme-default '
-                                      'controls_RichEditor_theme-default '
-                                      'richEditor_richContentRender_theme-default '
-                                      'richEditor_richContentRender '
-                                      'richEditor_richContentRender_lineHeight-s '
-                                      'richEditor_richContentRender_colorPalette-first '
-                                      'richEditor_richContentRender_readOnly msg-dialogs-item__message-text '
-                                      'msg-entity-text_normalized controls-List_DragNDrop__notDraggable '
-                                      'ws-flex-shrink-1 msg-entity-expander__content '
-                                      'ws-flex-grow-1 ws-flex-shrink-1"]').click()
+    if driver.find_element(By.XPATH, '//div[@data-qa="items-container"]//p').text == 'Текстовое сообщение':
+        driver.find_element(By.XPATH, '//div[@data-qa="items-container"]//p').click()
         time.sleep(2)
         driver.find_element(By.XPATH, '//div[@data-qa="remove"]').click()
         time.sleep(2)
         try:
-            driver.find_element(By.XPATH, '//div[@class="msg-entity-text msg-entity_font_croppless '
-                                         'richEditor_richContentRender_fontSize-m_theme-default '
-                                         'controls_RichEditor_theme-default '
-                                         'richEditor_richContentRender_theme-default '
-                                         'richEditor_richContentRender '
-                                         'richEditor_richContentRender_lineHeight-s '
-                                         'richEditor_richContentRender_colorPalette-first '
-                                         'richEditor_richContentRender_readOnly msg-dialogs-item__message-text '
-                                         'msg-entity-text_normalized controls-List_DragNDrop__notDraggable '
-                                         'ws-flex-shrink-1 msg-entity-expander__content '
-                                         'ws-flex-grow-1 ws-flex-shrink-1"]').text == 'Тестовое сообщение'
+            driver.find_element(By.XPATH, '//div[@data-qa="items-container"]//p').text == 'Тестовое сообщение'
         except NoSuchElementException:
             driver.execute_script('alert(arguments[0])', 'Сообщение удалено')
             time.sleep(2)
